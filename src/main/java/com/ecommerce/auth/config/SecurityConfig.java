56a33873e8f1242a73048e5dd178330066a3d942
Petar Mitov <ttheconjuring@gmail.com>
2025-08-07 18:43:21 +0300

    Synchronise with initial repository
    Update this repository according to the latest main branch from the initial repository.

--- src/main/java/com/ecommerce/auth/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/auth/config/SecurityConfig.java
@@ -28,11 +28,9 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-
+                .authorizeHttpRequests(auth ->
+                        // TODO: For development/testing purpose only. Change on prod.
+                        auth.requestMatchers("/**").permitAll());
         http.authenticationProvider(authenticationProvider());
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
